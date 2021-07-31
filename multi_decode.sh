daba9bb1afb8098191b9329ba9256a2db0ce5bae
crow <behruzfox@mail.ru>
2021-07-31 12:28:13 -0400

    All is OK

--- multi_decode.sh
+++ multi_decode.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-in_dir=$1
-out_dir=$2
-help_mesg="Please enter IN and OUT directories!\nFor example: multi_decode.sh ~/wav_files ~/out_files"
+out_dir=$1
+help_mesg="Пожалуйста укажите путь к IN и OUT каталогам !\nНапример: ./multi_decode.sh  ~/out_files"
 
 case "$1" in
 -h | --help | help) echo -e $help_mesg ;;
 esac
 
 
-if [ -z $1 ] || [ -z $2 ]
+if [ -z $1 ]
 then
 	echo -e $help_mesg
 	exit
 
 fi
 
-for my_file in $(ls $1)
+
+if ! [ -d $1 ]
+then 
+	echo "Каталог $2 не существует"
+	exit
+fi
+
+for my_file in $(ls *.wav)
 do
-	if [ -f $my_file ]
-	then
-#		$(pwd)/decode.sh $1/ > "$2/$my_file.txt"
-#		rm $file
-	fi
+	my_file_no_type=$(echo $my_file | cut -f 1 -d '.')
+	echo "$my_file_no_type $my_file" > decoder-test.scp
+	echo "$my_file_no_type $my_file_no_type" > decoder-test.utt2spk 
+	./decode.sh  &> "$1/$my_file_no_type.txt"
+
+#auto delete wav files
+	rm $my_file                     
 
 done
